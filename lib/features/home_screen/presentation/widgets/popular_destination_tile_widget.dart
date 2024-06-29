be23bef0919046d2e004dbb6e63a8bf9190bf204
Yash Vishwakarma <yash@umeed.health>
2024-06-29 19:25:07 +0530

    Done Linking

--- lib/features/home_screen/presentation/widgets/popular_destination_tile_widget.dart
+++ lib/features/home_screen/presentation/widgets/popular_destination_tile_widget.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:on_a_trip/common/constants/colors.dart';
+import 'package:on_a_trip/features/destination_screen/presentation/screens/proposal_detail_screen.dart';
 
 class PopularDestinationTileWidget extends StatelessWidget {
   const PopularDestinationTileWidget({
@@ -17,80 +18,87 @@ class PopularDestinationTileWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(right: 18.h),
-      child: CachedNetworkImage(
-        imageUrl: imageUrl,
-        imageBuilder: (context, imageProvider) => Container(
-          width: 150,
-          height: 200,
-          padding: const EdgeInsets.all(8),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12),
-            image: DecorationImage(
-              image: imageProvider,
-              fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => const ProposalDetailScreen()),
+        );
+      },
+      child: Padding(
+        padding: EdgeInsets.only(right: 18.h),
+        child: CachedNetworkImage(
+          imageUrl: imageUrl,
+          imageBuilder: (context, imageProvider) => Container(
+            width: 150,
+            height: 200,
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              image: DecorationImage(
+                image: imageProvider,
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Material(
-                color: Colors.transparent,
-                borderRadius: BorderRadius.circular(10),
-                child: Container(
-                  padding: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.85),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        title,
-                        style: const TextStyle(
-                          color: CustomColors.titleColor,
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Material(
+                  color: Colors.transparent,
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.85),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          title,
+                          style: const TextStyle(
+                            color: CustomColors.titleColor,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      const SizedBox(height: 2),
-                      const Row(
-                        children: [
-                          Icon(
-                            Icons.location_on_outlined,
-                            size: 12,
-                            color: CustomColors.primaryColor,
-                          ),
-                          SizedBox(width: 4),
-                          Text(
-                            "South Tyrol, Italy",
-                            style: TextStyle(
+                        const SizedBox(height: 2),
+                        const Row(
+                          children: [
+                            Icon(
+                              Icons.location_on_outlined,
+                              size: 12,
                               color: CustomColors.primaryColor,
-                              fontSize: 12,
-                              fontWeight: FontWeight.w500,
                             ),
-                          ),
-                        ],
-                      ),
-                    ],
+                            SizedBox(width: 4),
+                            Text(
+                              "South Tyrol, Italy",
+                              style: TextStyle(
+                                color: CustomColors.primaryColor,
+                                fontSize: 12,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
+              ],
+            ),
+          ),
+          placeholder: (context, url) => Container(
+            width: 150,
+            height: 200,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: Colors.grey.shade200,
+            ),
+            child: const Center(
+              child: CircularProgressIndicator(
+                color: CustomColors.primaryColor,
               ),
-            ],
-          ),
-        ),
-        placeholder: (context, url) => Container(
-          width: 150,
-          height: 200,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12),
-            color: Colors.grey.shade200,
-          ),
-          child: const Center(
-            child: CircularProgressIndicator(
-              color: CustomColors.primaryColor,
             ),
           ),
         ),
--- lib/features/home_screen/presentation/widgets/recommendation_tile_wdiget.dart
+++ lib/features/home_screen/presentation/widgets/recommendation_tile_wdiget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:on_a_trip/common/constants/colors.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:on_a_trip/features/destination_screen/presentation/screens/proposal_detail_screen.dart';
 
 class RecommendationTileWidget extends StatelessWidget {
   const RecommendationTileWidget({
@@ -14,116 +15,123 @@ class RecommendationTileWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 12.0),
-      child: Row(
-        children: [
-          CachedNetworkImage(
-            imageUrl: imgUrl,
-            imageBuilder: (context, imageProvider) => Container(
-              width: 72,
-              height: 72,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                image: DecorationImage(
-                  image: imageProvider,
-                  fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => const ProposalDetailScreen()),
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 12.0),
+        child: Row(
+          children: [
+            CachedNetworkImage(
+              imageUrl: imgUrl,
+              imageBuilder: (context, imageProvider) => Container(
+                width: 72,
+                height: 72,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  image: DecorationImage(
+                    image: imageProvider,
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
-            ),
-            placeholder: (context, url) => Container(
-              width: 72,
-              height: 72,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                color: Colors.grey.shade200,
-              ),
-              child: const Center(
-                child: CircularProgressIndicator(
-                  color: CustomColors.primaryColor,
+              placeholder: (context, url) => Container(
+                width: 72,
+                height: 72,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Colors.grey.shade200,
                 ),
-              ),
-            ),
-          ),
-          const SizedBox(width: 12),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                title,
-                style: const TextStyle(
-                  color: CustomColors.titleColor,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 6),
-              const Row(
-                children: [
-                  Icon(
-                    Icons.location_on_outlined,
-                    size: 12,
+                child: const Center(
+                  child: CircularProgressIndicator(
                     color: CustomColors.primaryColor,
                   ),
-                  SizedBox(width: 4),
-                  Text(
-                    "South Tyrol, Italy",
-                    style: TextStyle(
-                      color: CustomColors.primaryColor,
-                      fontSize: 12,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          const Spacer(),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Row(
-                children: [
-                  Icon(
-                    Icons.star_rounded,
-                    color: Colors.yellow.shade700,
-                    size: 18,
-                  ),
-                  Icon(
-                    Icons.star_rounded,
-                    color: Colors.yellow.shade700,
-                    size: 18,
-                  ),
-                  Icon(
-                    Icons.star_rounded,
-                    color: Colors.yellow.shade700,
-                    size: 18,
-                  ),
-                  Icon(
-                    Icons.star_rounded,
-                    color: Colors.yellow.shade700,
-                    size: 18,
-                  ),
-                  Icon(
-                    Icons.star_half_rounded,
-                    color: Colors.yellow.shade700,
-                    size: 18,
-                  ),
-                ],
-              ),
-              const SizedBox(width: 6),
-              const Text(
-                "4.5 of 5",
-                style: TextStyle(
-                  color: Colors.black87,
-                  fontSize: 12,
-                  fontWeight: FontWeight.w500,
                 ),
               ),
-            ],
-          ),
-        ],
+            ),
+            const SizedBox(width: 12),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                    color: CustomColors.titleColor,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 6),
+                const Row(
+                  children: [
+                    Icon(
+                      Icons.location_on_outlined,
+                      size: 12,
+                      color: CustomColors.primaryColor,
+                    ),
+                    SizedBox(width: 4),
+                    Text(
+                      "South Tyrol, Italy",
+                      style: TextStyle(
+                        color: CustomColors.primaryColor,
+                        fontSize: 12,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const Spacer(),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Row(
+                  children: [
+                    Icon(
+                      Icons.star_rounded,
+                      color: Colors.yellow.shade700,
+                      size: 18,
+                    ),
+                    Icon(
+                      Icons.star_rounded,
+                      color: Colors.yellow.shade700,
+                      size: 18,
+                    ),
+                    Icon(
+                      Icons.star_rounded,
+                      color: Colors.yellow.shade700,
+                      size: 18,
+                    ),
+                    Icon(
+                      Icons.star_rounded,
+                      color: Colors.yellow.shade700,
+                      size: 18,
+                    ),
+                    Icon(
+                      Icons.star_half_rounded,
+                      color: Colors.yellow.shade700,
+                      size: 18,
+                    ),
+                  ],
+                ),
+                const SizedBox(width: 6),
+                const Text(
+                  "4.5 of 5",
+                  style: TextStyle(
+                    color: Colors.black87,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
